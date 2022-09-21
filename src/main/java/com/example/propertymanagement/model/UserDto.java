6c2859d9809fca499021474ce68501d14976d444
wh173d3v11 <dineshlakhzz@gmail.com>
2022-09-21 17:38:04 +0530

    * JsonIgnoreProperties and JsonInclude - sending only needed variables and ingore all null variables.

--- src/main/java/com/example/propertymanagement/model/UserDto.java
+++ src/main/java/com/example/propertymanagement/model/UserDto.java
@@ -1,10 +1,14 @@
 package com.example.propertymanagement.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDto {
     private Long id;
     private String ownerName;
