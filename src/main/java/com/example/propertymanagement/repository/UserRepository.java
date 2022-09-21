e2f034088ccf944076fddd331db4e0d18401c010
wh173d3v11 <dineshlakhzz@gmail.com>
2022-09-21 18:09:24 +0530

    * Custom JPQL query and FindBy method

--- src/main/java/com/example/propertymanagement/repository/UserRepository.java
+++ src/main/java/com/example/propertymanagement/repository/UserRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 
     //custom findBy methods
-    //we can use JPQL query also. check here - https://springframework.guru/spring-data-jpa-query/
+    //we can also use JPQL query also. check here - https://springframework.guru/spring-data-jpa-query/
     Optional<UserEntity> findByOwnerEmailAndPassword(String email, String password);
 }
