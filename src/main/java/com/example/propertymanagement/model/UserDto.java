c9f3b13f3db2c09b4540d23c75dae1a0bec04fac
wh173d3v11 <dineshlakhzz@gmail.com>
2022-09-25 18:07:07 +0530

    * Implemented Field level Validations (Hibernate Validators)

--- src/main/java/com/example/propertymanagement/model/UserDto.java
+++ src/main/java/com/example/propertymanagement/model/UserDto.java
@@ -5,14 +5,24 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonIgnoreProperties(ignoreUnknown = true) //JsonIgnoreProperties and JsonInclude - sending only needed variables and ignore all null variables.
+@JsonIgnoreProperties(ignoreUnknown = true)
+//JsonIgnoreProperties and JsonInclude - sending only needed variables and ignore all null variables.
 public class UserDto {
     private Long id;
     private String ownerName;
+    @NotNull(message = "Owner Email is mandatory") //hibernate Validator
+    @NotEmpty(message = "Owner Email cannot be Empty")
+    @Size(min = 1, max = 50, message = "Owner Email Should be between 1 to 50 Characters in length")
     private String ownerEmail;
     private String phone;
+    @NotNull(message = "Password cannot be null")
+    @NotEmpty(message = "Password cannot be Empty")
     private String password;
 }
